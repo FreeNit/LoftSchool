9f0dca40c717a567326b1228066e9030e3b2a635
Dmytro Bolilyi <Bolilui.Dmytro@gmail.com>
2020-01-30 02:19:42 +0200

    homework -1 DONE

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(firstArgument) {
+    return firstArgument;
 }
 
 /*
@@ -29,7 +30,8 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
+function sumWithDefaults(a, b = 100) {
+    return a + b;
 }
 
 /*
@@ -41,6 +43,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    return fn();
 }
 
 /*
@@ -56,7 +59,12 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {
+function returnCounter(number = 0) {
+    var newNumber = function() {
+        return ++number;
+    }
+
+    return newNumber;
 }
 
 /*
@@ -68,8 +76,23 @@ function returnCounter(number) {
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
+
 function returnArgumentsArray() {
+    var arr = Array.prototype.slice.call(arguments);
+    
+    return arr;
 }
+
+// ***** Rest parameter ***** //
+//   function returnCounter(...numbers) {
+//   return numbers
+// }
+
+// ***** Array.from ***** //
+// function returnCounter() {
+//   let arr = Array.from(arguments);
+//   return arr;
+// }
 
 /*
  Задание 6 *:
@@ -86,7 +109,17 @@ function returnArgumentsArray() {
 
    console.log(newSum()) выведет 6
  */
+
 function bindFunction(fn) {
+    var args = '';
+
+    for (var i = 1; i < arguments.length; i++) {
+        args += arguments[i];
+    }
+
+    var func = fn.bind(null, args);
+
+    return func;
 }
 
 export {
