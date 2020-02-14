13bf5d0b9afcc453d22f26412682b3f3cf5608c9
Dmytro Bolilyi <Bolilui.Dmytro@gmail.com>
2020-02-14 14:36:44 +0200

    remove index.js from TEST folder

--- src/dnd.js
+++ src/dnd.js
@@ -27,6 +27,41 @@ const homeworkContainer = document.querySelector('#homework-container');
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+        
+    let maxWidth = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+
+    let maxHeight = Math.max(
+        document.body.scrollWidth, document.documentElement.scrollWidth,
+        document.body.offsetWidth, document.documentElement.offsetWidth,
+        document.body.clientWidth, document.documentElement.clientWidth
+    );
+
+    function getRandomColor() {
+        let color = '';
+
+        for (let i = 0; i < 3; i++) {
+            let sub = Math.floor(Math.random() * 256).toString(16);
+
+            color += (sub.length === 1 ? '0' + sub : sub);
+        }
+  
+        return `#${color}`;
+    }
+
+    let divElem = document.createElement('div'); // Create new DIV tag
+
+    divElem.classList.add('draggable-div'); // Add 'draggable-div' class 
+    divElem.style.backgroundColor = getRandomColor(); // Add random color
+    divElem.style.width = Math.random() * maxWidth + 'px'; // Add random width
+    divElem.style.height = Math.random() * maxHeight + 'px'; // Add random height
+    divElem.style.left = Math.random() * maxWidth + 'px'; // Add random horizontal position
+    divElem.style.top = Math.random() * maxHeight + 'px'; // Add random vertical position
+
+    return divElem;
 }
 
 /*
@@ -38,6 +73,38 @@ function createDiv() {
    addListeners(newDiv);
  */
 function addListeners(target) {
+    target.onmousedown = function(event) { // (1) отследить нажатие
+        // (2) подготовить к перемещению:
+        // разместить поверх остального содержимого и в абсолютных координатах
+        target.style.position = 'absolute';
+        target.style.zIndex = 1000;
+        // переместим в body, чтобы target был точно не внутри position:relative
+        document.body.append(target);
+        // и установим абсолютно спозиционированный target под курсор
+    
+        moveAt(event.pageX, event.pageY);
+    
+        // передвинуть target под координаты курсора
+        // и сдвинуть на половину ширины/высоты для центрирования
+        function moveAt(pageX, pageY) {
+            target.style.left = pageX - target.offsetWidth / 2 + 'px';
+            target.style.top = pageY - target.offsetHeight / 2 + 'px';
+        }
+    
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+    
+        // (3) перемещать по экрану
+        document.addEventListener('mousemove', onMouseMove);
+    
+        // (4) положить target, удалить более ненужные обработчики событий
+        target.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            target.onmouseup = null;
+        };
+    
+    };
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
